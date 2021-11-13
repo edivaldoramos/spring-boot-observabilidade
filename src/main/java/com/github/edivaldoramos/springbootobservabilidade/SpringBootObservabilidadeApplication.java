b4beeb3845b985ad0529600a612c15057fa84404
edivaldo <edivaldo.ramos@grupomateus.com.br>
2021-11-12 21:26:58 -0300

    Adiciona documentação da applicação

---
+++ src/main/java/com/github/edivaldoramos/springbootobservabilidade/SpringBootObservabilidadeApplication.java
@@ -0,0 +1,13 @@
+package com.github.edivaldoramos.springbootobservabilidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootObservabilidadeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootObservabilidadeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/github/edivaldoramos/springbootobservabilidade/SpringBootObservabilidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.edivaldoramos.springbootobservabilidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootObservabilidadeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
